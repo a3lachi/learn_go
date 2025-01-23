e9da2b6ab5cd7140c0a7d1694180b3e436c394ba
a3laxi <mad.aalachi@gmail.com>
2025-01-23 22:36:23 +0100

    CRON job

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import "time"
 
 func firstFunc() {
 	fmt.Println("Kira7 a7ssen artist fi maruecos")
-	fmt.Println("3aaaaasseeemaaa")
+	fmt.Println("3aaaaasseeeemaaa")
 }
 
 func multiply(x int, y int) int {
